402f374773279fc313ac6900f3ef8d4f81aa07dd
Glen Jarvis <glen@glenjarvis.com>
2012-01-02 10:30:22 -0800

    Add crude cronjob to poll mechanical turk
    This is an only roughly tested Django command that can act as a cron job
    to periodically poll Amazon Mechanical Turk. The time should be set to
    configurable in the settings file for a future iteration.

--- helpers.py
+++ helpers.py
@@ -5,19 +5,22 @@ from djurk.common import get_connection
 from djurk.models import HIT
 
 
-def _update_hits(iterable):
+def _update_hits(iterable, do_update_assignments=False):
     for mturk_hit in iterable:
         djurk_hit = HIT.objects.get_or_create(mturk_id=mturk_hit.HITId)[0]
-        djurk_hit.update(mturk_hit=mturk_hit)
+        djurk_hit.update(mturk_hit=mturk_hit,
+                         do_update_assignments=do_update_assignments)
 
-
-def update_all_hits():
+def update_all_hits(do_update_assignments=False):
     """Get All HITS from Amazon"""
     connection = get_connection()
-    _update_hits(connection.get_all_hits())
+    _update_hits(connection.get_all_hits(),
+                 do_update_assignments=do_update_assignments)
 
 
-def update_reviewable_hits():
+def update_reviewable_hits(do_update_assignments=False):
+
     """Get only reviewable HITS from Amazon"""
     connection = get_connection()
-    _update_hits(connection.get_reviewable_hits())
+    _update_hits(connection.get_reviewable_hits(),
+                 do_update_assignments=do_update_assignments)
---
+++ management/commands/poll_mturk.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Cron job to constantly poll Amazon Mechanical Turk"""
+
+import logging
+import time
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from djurk.common import get_connection
+from djurk.helpers import update_all_hits, update_reviewable_hits
+
+SLEEP_TIME = 5 * 60  # 5 minutes
+
+
+class NullHandler(logging.Handler):
+    """Create silent logger to avoid confusion for new programmers
+
+    If logging is needed, it can be passed into this cronjob. However,
+    if logging isn't provided, a message such as "No handlers could be
+    found for logger..." is often printed. This adds confusion.
+
+    This is avoided by defining this handler that does not log. A
+    logging handler can be substituted instead if the output is
+    needed.
+    """
+    def emit(self, record):
+        pass
+
+logger = logging.getLogger("djurk").addHandler(NullHandler())
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option(
+        '--assignments',
+        action='store_true',
+        dest='do_update_assignments',
+        default=False,
+        help='Update Assignments as well as HITs (more intensive)'),
+        make_option(
+        '--reviewable',
+        action='store_true',
+        dest='reviewable',
+        default=False,
+        help=('Only update reviewable HITs (less demand on MTurk - '
+              ' possibly miss HITs)')),
+        make_option(
+        '--sandbox',
+        action='store_true',
+        dest='sandbox',
+        default=False,
+        help='Use Amazon Mechanical Turk Sandbox (instead of production)'),
+    )
+
+    def handle(self, *args, **options):
+        use_sandbox = options['sandbox']
+        # Warning: This is a class level connection. If this is different
+        #          than other instances running, their connections will
+        #          also suddenly update
+        #          See comments in get_connection
+        mtc = get_connection(use_sandbox=use_sandbox)
+        do_update_assignments = options['do_update_assignments']
+
+        while True:
+            if options['reviewable']:
+                logging.info(("Updating Reviewable HITs with "
+                              "Assignments: %s") % do_update_assignments)
+                update_reviewable_hits(
+                        do_update_assignments=do_update_assignments)
+            else:
+                logging.info(("Updating All HITs with "
+                              "Assignments: %s") % do_update_assignments)
+                update_all_hits(
+                        do_update_assignments=do_update_assignments)
+            logging.info("Sleeping")
+            time.sleep(SLEEP_TIME)
