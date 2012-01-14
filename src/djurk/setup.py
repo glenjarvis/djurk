995e77f201b8dc37199dd14154408a0acda65edc
Mike Edwards <mike@onearmedman.com>
2012-01-14 18:55:44 -0500

    Added a basic setup file

---
+++ src/djurk/setup.py
@@ -0,0 +1,27 @@
+'''
+Created on Jan 10, 2012
+
+@author: Mike_Edwards
+'''
+from distutils.core import setup
+
+setup(name='django-savvy',
+      version='0.01.01-Alpha',
+      description='A Django app for accessing Mechanical Turk',
+      long_description=open('README.markdown').read(),
+      author='Glen Jarvis',
+      url='http://djurk.org/',
+      license='BSD',
+      packages=['djurk', 
+                'djurk.migrations',
+                'djurk.management'],
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
+     )
